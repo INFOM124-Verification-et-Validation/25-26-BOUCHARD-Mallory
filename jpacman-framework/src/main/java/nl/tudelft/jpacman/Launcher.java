1f9f542cf734dd2eeee61752c3afca9e7299ee9d
mallobcd <m2bc2d.pro@gmail.com>
2025-11-27 11:41:32 +0100

    Add SonarCloud organization to build configuration

--- jpacman-framework/src/main/java/nl/tudelft/jpacman/Launcher.java
+++ jpacman-framework/src/main/java/nl/tudelft/jpacman/Launcher.java
@@ -31,7 +31,6 @@ public class Launcher {
 
     public static final String DEFAULT_MAP = "/board.txt";
     private String levelMap = DEFAULT_MAP;
-
     private PacManUI pacManUI;
     private Game game;
 
